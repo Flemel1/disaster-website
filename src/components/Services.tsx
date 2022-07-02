eac1397fbb45258fadfaf9fafe8916d27d530aa3
Flemel1 <lelurezt@gmail.com>
2022-07-02 15:46:59 +0700

    homepage

---
+++ src/components/Services.tsx
@@ -0,0 +1,23 @@
+const Services = () => {
+  return (
+    <div className="service">
+      <h2>Layanan yang diberikan ChiFlo</h2>
+      <div className="service__wrapper">
+        <div className="service__item">
+          <img src="/favicon.ico" alt="icon" />
+          <p>Memberikan Informasi Terkait Banjir</p>
+        </div>
+        <div className="service__item">
+          <img src="/favicon.ico" alt="icon" />
+          <p>Jasa Pembersihan Rumah & Properti</p>
+        </div>
+        <div className="service__item">
+          <img src="/favicon.ico" alt="icon" />
+          <p>Asuransi Aset & Properti</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Services
